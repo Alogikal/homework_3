5cebcea6fa6c830a9c02b72ccb26efebd6cd4c8e
Alogikal <andi.alogikal@mail.com>
2024-09-22 01:34:34 +0300

    Сдал домашку

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,9 @@ public void main() {
     long d = 987678965549L;
     float e = 2.786f;
     double f = 27.12;
+
     System.out.println("");
-    System.out.println("var = " + a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n" + f + "\n");
+    System.out.println("Значение переменной a с типом данных integer = " + a + "\n" + "Значение переменной b с типом данных integer = " + b + "\n" + "Значение переменной c с типом данных integer = " + c + "\n" + "Значение переменной d с типом данных integer = " + d + "\n" + "Значение переменной e с типом данных integer = " + e + "\n" + "Значение переменной f с типом данных integer = " + f + "\n");
 
     int lyudmila = 23;
     int anna = 27;
@@ -15,18 +16,21 @@ public void main() {
     int sheets = 480;
 
     System.out.println("");
+    System.out.println("Значение переменной lyudmila с типом данных integer = " + lyudmila + "\n" + "Значение переменной anna с типом данных integer = " + anna + "\n" + "Значение переменной ekaterina с типом данных integer = " + ekaterina + "\n");
     System.out.println("На каждого ученика рассчитано " + (sheets / (lyudmila + anna + ekaterina)) +  " листов бумаги");
 
     int efficiency = 16;
-    int time1 = 20;
+    float time1 = 20;
     int time2 = 24 * 60;
     int time3 = 24 * 3 * 60;
     int time4 = 24 * 30 * 60;
 
-    System.out.println("За " + time1 + " минут машина произвела " + (time1 / efficiency) + " штук бутылок");
-    System.out.println("За " + time2 + " минут машина произвела " + (time2 / efficiency) +" штук бутылок");
-    System.out.println("За " + time3 + " минут машина произвела " + (time3 / efficiency) +" штук бутылок");
-    System.out.println("За " + time4 + " минут машина произвела " + (time3 / efficiency) +" штук бутылок");
+    System.out.println("");
+    System.out.println("Значение переменной time1 с типом float равно " + time1 + "\n" + "Значение переменной time2 с типом int равно " + time2 + "\n" + "Значение переменной time3 с типом int равно " + time3 + "\n" + "Значение переменной time4 с типом int равно " + time4 + "\n");
+    System.out.println("За " + time1 + " минут машина произвела " + (time1 / efficiency / 2) + " штук бутылок");
+    System.out.println("За " + time2 + " минут машина произвела " + (time2 / efficiency / 2) +" штук бутылок");
+    System.out.println("За " + time3 + " минут машина произвела " + (time3 / efficiency / 2) +" штук бутылок");
+    System.out.println("За " + time4 + " минут машина произвела " + (time4 / efficiency / 2) +" штук бутылок");
 
     int potsWhiteAndBrown = 120;
     int potsWhite = 6;
@@ -44,9 +48,10 @@ public void main() {
     int iceCreamWeight = 2 * 100;
     int eggs = 4;
     int eggsWeight = 70 * eggs;
-    int breakfast = bananasWeight + weightMilk + eggsWeight + iceCreamWeight;
+    float breakfast = bananasWeight + weightMilk + eggsWeight + iceCreamWeight;
 
     System.out.println("");
-    System.out.println("Вес завтрака спротсмена в граммах равен " + breakfast + " или " + (breakfast * 0.77) + " килограмм");
+    System.out.println("Значение переменной bananas с типом int равно " + bananas + "\n" + "Значение переменной milk с типом int равно " + milk + "\n" + "Значение переменной icecream с типом int равно " + iceCream + "\n" + "Значение переменной eggs с типом int равно " + eggs + "\n");
+    System.out.println("Вес завтрака спротсмена в граммах равен " + breakfast + " или " + (breakfast / 1000) + " килограмм");
 
 }
